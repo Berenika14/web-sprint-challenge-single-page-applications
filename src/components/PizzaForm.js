291f1667c3503e59f9b21bc25552117b7b5e09a3
Berenika Ahmetaj <ahmetajberenika@gmail.com>
2021-12-17 16:11:30 -0500

    added the validation form for the name input

--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,13 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import * as yup from "yup";
+import schema from "../validation/FormSchema";
+
+const initialFormState = {
+  name: " ",
+};
+const initialFormErrors = {
+  name: " ",
+};
 
 const PizzaForm = () => {
+  const [form, setForm] = useState(initialFormState);
+  const [errors, setErrors] = useState(initialFormErrors);
+  console.log(errors);
+  const validate = (name, value) => {
+    yup
+      .reach(schema, name)
+      .validate(value)
+      .then(() => {
+        console.log("name is valid");
+        setErrors({ ...errors, [name]: "" });
+      })
+      .catch((err) => {
+        console.log(err);
+        setErrors({ ...errors, [name]: err.errors[0] });
+      });
+  };
+
+  const changeHandler = (ev) => {
+    const value =
+      ev.target.type === "checkbox" ? ev.target.checked : ev.target.value;
+    setForm({ ...form, [ev.target.name]: value });
+    validate(ev.target.name, value);
+  };
+
   return (
     <div>
       <form id="pizza-form">
         <header>
           <h1>Build your own Pizza</h1>
         </header>
-        <input id="name-input" />
+        <div className="errors">{errors.name}</div>
+        <input
+          onChange={changeHandler}
+          name="name"
+          type="text"
+          id="name-input"
+        />
       </form>
     </div>
   );
